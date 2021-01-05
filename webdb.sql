99723204fd65ebe92681e913477440a3c5839bb7
이다현 <74286308+C0RVU55@users.noreply.github.com>
2021-01-05 17:47:37 +0900

    0

---
+++ webdb.sql
@@ -0,0 +1,21 @@
+--테이블 / 시퀀스 삭제
+drop table guestbook;
+drop SEQUENCE seq_no;
+
+commit;
+rollback;
+
+--테이블 생성
+create table guestbook(
+    no number,
+    name VARCHAR2(80),
+    password VARCHAR2(20),
+    content VARCHAR2(2000),
+    reg_date date,
+    PRIMARY KEY (no)
+);
+
+--시퀀스 생성
+create SEQUENCE seq_no
+  INCREMENT BY 1
+  start with 1;
